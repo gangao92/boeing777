1430f4e771dfc9b697af94b48350890b9424512e
ggao <ggao@simulmedia.com>
2017-11-07 22:12:31 -0500

    add API references

--- GetPriceData.py
+++ GetPriceData.py
@@ -3,6 +3,10 @@ import quandl
 quandl.ApiConfig.api_key = ""
 
 # price data for Boeing Co.
+
+# References:
+# https://www.quandl.com/product/WIKIP/usage/quickstart/python
+# https://docs.quandl.com/docs/python-tables
 price_data = quandl.get_table('WIKI/PRICES', ticker='BA', date={'gte': '2016-01-01', 'lte': '2016-12-31'}, paginate=True)
 
 # key words for Boeing Co.
